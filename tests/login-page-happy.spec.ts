fe143dd544c50c0b1eba41fbb5467efd6d383581
Daniel Lister <daniel.lister@coveainsurance.co.uk>
2024-06-17 21:10:48 +0100

    Feature: Login page social media icons

--- tests/login-page-happy.spec.ts
+++ tests/login-page-happy.spec.ts
@@ -30,12 +30,82 @@ test('A previously registered user successfully logs in to the Orange HRM servic
 	await expect(page).toHaveURL(
 		'https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index',
 	);
+
+	await page.close();
 });
 
-test('A user attempts to use the LinkedIn link on the login page', async ({ page }) => {});
+test('A user attempts to use the LinkedIn link on the login page', async ({
+	base,
+	loginPage,
+	page,
+}) => {
+	await base.navigateToUrl('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
 
-test('A user attempts to use the Twitter/X link on the login page', async ({ page }) => {});
+	const [linkedInPage] = await Promise.all([
+		page.waitForEvent('popup'),
+		loginPage.pressLinkedInButton(),
+	]);
+	await linkedInPage.waitForLoadState();
 
-test('A user attempts to use the Facebook link on the login page', async ({ page }) => {});
+	await expect(linkedInPage).toHaveURL('https://www.linkedin.com/company/orangehrm');
+	await linkedInPage.close();
+	await page.close();
+});
 
-test('A user attempts to use the YouTube link on the login page', async ({ page }) => {});
+test('A user attempts to use the Twitter/X link on the login page', async ({
+	base,
+	loginPage,
+	page,
+}) => {
+	await base.navigateToUrl('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
+
+	const [twitterPage] = await Promise.all([
+		page.waitForEvent('popup'),
+		loginPage.pressTwitterButton(),
+	]);
+	await twitterPage.waitForLoadState();
+
+	await expect(twitterPage).toHaveURL('https://x.com/orangehrm?lang=en');
+	await twitterPage.close();
+	await page.close();
+});
+
+test('A user attempts to use the Facebook link on the login page', async ({
+	base,
+	loginPage,
+	page,
+}) => {
+	await base.navigateToUrl('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
+
+	const [facebookPage] = await Promise.all([
+		page.waitForEvent('popup'),
+		loginPage.pressFacebookButton(),
+	]);
+	await facebookPage.waitForLoadState();
+
+	await expect(facebookPage).toHaveURL('https://www.facebook.com/OrangeHRM/');
+	await facebookPage.close();
+	await page.close();
+});
+
+test('A user attempts to use the YouTube link on the login page', async ({
+	base,
+	loginPage,
+	page,
+}) => {
+	await base.navigateToUrl('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
+
+	const [youTubePage] = await Promise.all([
+		page.waitForEvent('popup'),
+		loginPage.pressYouTubeButton(),
+	]);
+	await youTubePage.waitForLoadState();
+
+	const orangeHome = youTubePage.getByTitle('(1) OrangeHRM Inc - YouTube', { exact: true });
+	const consentPage = youTubePage.getByTitle('Before you continue to YouTube', { exact: true });
+
+	expect(orangeHome.or(consentPage)).toBeDefined();
+
+	await youTubePage.close();
+	await page.close();
+});
