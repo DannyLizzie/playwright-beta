73d59b0bb729a7f571f974fc81d39f7fa7e3c3d9
Daniel Lister <62890806+DannyLizzie@users.noreply.github.com>
2024-06-20 13:36:07 +0100

    Feature: User inputs empty string into forgotten password username field (#9)
    Co-authored-by: Daniel Lister <daniel.lister@coveainsurance.co.uk>

--- tests/pages/password-reset-page/generic.spec.ts
+++ tests/pages/password-reset-page/generic.spec.ts
@@ -32,3 +32,31 @@ test(
 		await expect(page).toHaveURL('/web/index.php/auth/login');
 	},
 );
+
+test.describe(
+	'Feat(OHRM-022): User inputs empty string when requesting password reset',
+	{ tag: ['@generic-user', '@password-reset-page'] },
+	() => {
+		test(
+			'An invalid user attempts to login with an unknown password only',
+			{
+				annotation: {
+					type: 'Acceptance Criteria',
+					description:
+						'AC1: The one where a user attempts to request a password reset with an empty string',
+				},
+			},
+			async ({ base, loginPage, page, passwordResetPage }) => {
+				await base.navigateToUrl('/web/index.php/auth/login');
+
+				await loginPage.pressForgotPasswordButton(),
+					expect(page).toHaveURL('/web/index.php/auth/requestPasswordResetCode');
+
+				await passwordResetPage.fillUserNameWithEmptyString();
+				await passwordResetPage.pressCancelButton();
+
+				await expect(page).toHaveURL('/web/index.php/auth/login');
+			},
+		);
+	},
+);
